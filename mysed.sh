6d5b60ca1ffec1d59f3d5aa5c5b9d09c90bd2155
Jan Sarenik <jajomojo@gmail.com>
2026-02-03 11:00:13 +0000

    Add blog/15

--- mysed.sh
+++ mysed.sh
@@ -3,6 +3,7 @@
 sed \
  -e 's/"\([^"]\+\)\/"/"\1"/g' \
  -e 's|img/favicons|/img/favicons|g' \
+ -e 's|href=" ?"|href="/"|g' \
  -e 's|https://www.liederindex.de||g' \
  -e 's|/styl/\([0-9]\)|/styl/\1?v=1234|' \
  -e 's|https://www.sirenipismasvateho.cz/favicon.ico|/favicon.ico|' \
--- scripts/fixrootlink.sh
+++ scripts/fixrootlink.sh
@@ -1,4 +1,5 @@
-git grep 'href=""' | cut -d: -f1 | sort -u | while read file
+git grep 'href=""' | cut -d: -f1 | sort -u \
+  | grep -w -v scripts | while read file
 do
   sed 's|href=""|href="/"|g' $file | safecat.sh $file
 done
