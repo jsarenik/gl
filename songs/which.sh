1bdf224310d5abf3db231379e68ba10167e64a1b
Jan Sarenik <jajomojo@gmail.com>
2026-02-03 13:23:29 +0000

    Use GL in song header before number

--- songs/which.sh
+++ songs/which.sh
@@ -1,3 +1,6 @@
+# use:
+# seq 1 180 | while read a; do sh which.sh $a; done
+
 n=$(grep "^$1 " ../934346-a628.5.txt | awk '{print $2}')
 echo $n
-sed -i "s/<h1>$/<h1>$1/" $n/index.html
+sed -i "s/<h1>$/<h1>GL $1/" $n/index.html
