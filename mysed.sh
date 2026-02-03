9cdb43b5a589577aa3c3223873dd808f9c08134a
Jan Sarenik <jajomojo@gmail.com>
2026-02-03 11:05:15 +0000

    Rename blog/15 to *.html

--- mysed.sh
+++ mysed.sh
@@ -7,4 +7,5 @@ sed \
  -e 's|https://www.liederindex.de||g' \
  -e 's|/styl/\([0-9]\)|/styl/\1?v=1234|' \
  -e 's|https://www.sirenipismasvateho.cz/favicon.ico|/favicon.ico|' \
- -e '/og:image/s|https://www.sirenipismasvateho.cz||'
+ -e '/og:image/s|https://www.sirenipismasvateho.cz||' \
+ | dos2unix
